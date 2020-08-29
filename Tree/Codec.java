8da3aa1226af8dbe6c2bebcc9bbc51874bce2e0d
Lin1nGithub <feibu2728zhidao@163.com>
2020-08-29 17:53:51 +0800

    二叉树的序列化和反序列化

--- Tree/Codec.java
+++ Tree/Codec.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
 /**
  * 二叉树的序列化和反序列化
  * https://leetcode-cn.com/problems/serialize-and-deserialize-binary-tree/
@@ -15,29 +18,58 @@ public class Codec {
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
         StringBuffer sb = new StringBuffer();
-        traverse(root, sb);
+        sb.append("[");
+        doSerialize(root, sb);
+        sb.subSequence(0, sb.length() - 1); // 去掉最后一个逗号
+        sb.append("]");
         return sb.toString();
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
+        if (null == data || data.length() == 0){
+            return null;
+        }
+        data = data.replace("[","");
+        data = data.replace("]","");
+        String[] array = data.split(",");
+        LinkedList<String> nodes = new LinkedList<>(Arrays.asList(array));
 
-        return null;
+        return doDeserialize(nodes);
     }
 
-    void traverse(TreeNode node,StringBuffer sb){
+    void doSerialize(TreeNode node,StringBuffer sb){
 
         if (node == null){
 
-            sb.append(NULL);
+            sb.append(NULL).append(SEP);
+
+            return;
         }
 
         sb.append(node.val).append(SEP);
 
-        traverse(node.left,sb);
+        doSerialize(node.left,sb);
 
-
-        traverse(node.right,sb);
-
+        doSerialize(node.right,sb);
     }
+
+    TreeNode doDeserialize(LinkedList<String> nodes) {
+        if (nodes.isEmpty()){
+            return null;
+        }
+        String val = nodes.removeFirst();
+        if (val.equals(NULL)){
+            return null;
+        }
+
+        TreeNode root = new TreeNode(Integer.valueOf(val));
+
+        root.left = doDeserialize(nodes);
+
+        root.right = doDeserialize(nodes);
+
+        return root;
+    }
+
 }
